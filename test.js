0ba0e4ff71dde1bfce19f8fc596068c3fca90a50
Rebecca Barrett <barrett4467@gmail.com>
2019-10-07 20:40:45 -0600

    Game tracks letters input, stops when word has been completed

--- test.js
+++ test.js
@@ -1,52 +1,71 @@
 var inquirer = require("inquirer");
-var wordArr = ["ankle"];
+var wordArr = ["awkward", "bagpipes", "banjo", "bungler", "croquet", "crypt", "dwarves", "fervid", "fishhook", "fjord"];
 var chosenWord = wordArr[Math.floor(wordArr.length * Math.random())];
 var correctGuess = [];
 letters = [];
-
+var count = 0;
+var incorrectGuesses = 0;
+var celebrateArr = ["Celebrate Good Times!! Come On! You've won!!", "Look at you go Champ!!", "You’ve got it made!", "SUPER", "WINNER WINNER CHICKEN DINNER"];
+var celebrate = celebrateArr[Math.floor(Math.random() * celebrateArr.length)]
 
 for (var i = 0; i < chosenWord.length; i++){
-    var wordLength = chosenWord[i].length; 
     letters.push(chosenWord[i]);
 
 }
 function startGame (){
+   
     console.log("Chosen Word: " + chosenWord);
     console.log("Correct letter: " + correctGuess);
     console.log("letters: " + letters.join(", "));
     
     for (var i = 0; i < letters.length; i++){
-        correctGuess.push(" __ ");
+        correctGuess.push("__");
+    }
+    game();
+}
+
+function letterConst (){
+    
+    for (var i = 0; i < letters.length; i++){
+        var getIndex = letters.indexOf(userGuess);
+        if (userGuess === chosenWord[i]){
+            letters.splice(getIndex, 1, userGuess);
+        } 
+    }
+    
+    if (userGuess === letters[getIndex]){
+        correctGuess.splice(getIndex, 1, userGuess);
+        count++;
+    } else {
+        console.log("Incorrect guess! Try again.");
+        incorrectGuesses++;
+    }
+    
+    console.log(correctGuess.join(" "));
+}
+
+function stopGame(){
+    console.log("Game Over!!");
+}
+
+startGame();
+function game (){
+    if (count < letters.length || incorrectGuesses === 10){
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Choose a letter",
+                choices: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+                name: "letter"
+            }
+        ]).then(function(answer){
+            userGuess = answer.letter;
+            letterConst();
+            game();
+        })
+        console.log(incorrectGuesses);
+    } else{
+        console.log("======================");
+        console.log(celebrate);
     }
 }
-startGame();
-var userGuess = "a";
-
-for (var i = 0; i < chosenWord.length; i++){
-    var getIndex = letters.indexOf(userGuess);
-    if (userGuess === chosenWord[i]){
-        letters.splice(getIndex, 1, userGuess);
-    } 
-}
-console.log("Index: " + getIndex);
-
-if (userGuess === letters[getIndex]){
-    correctGuess.splice(getIndex, 1, userGuess);
-}
-
-console.log(correctGuess);
-// inquirer.prompt([
-//     {
-//         type: "list",
-//         message: "Choose a letter",
-//         choices: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-//         name: "letter"
-//     }
-// ]).then(function(answer){
-//     if (answer.letter === correctLetter){
-//         console.log("Yess You're right");
-//         console.log(word)
-//     } else {
-//         console.log("nope that's wrong");
-//     }
-// })
